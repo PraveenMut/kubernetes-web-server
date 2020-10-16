ae6fe12f5d363232d4f39cb36547fd99bfbecd62
praveen <praveen.mut@gmail.com>
2020-10-16 16:34:55 +1100

    Change literal string response to a templated string to match with  best practices

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("Kubia server is starting...");
 let handler = (req, res) => { 
   console.log("Received request from " + req.connection.remoteAddress);
   res.writeHead(200);
-  res.end("You've hit " + os.hostname() + " with MAC of " + os.networkInterfaces().en0[0].address + "\n");
+  res.end(`You've hit ${os.hostname()}`);
 }
 let www = http.createServer(handler);
 www.listen(8080);
